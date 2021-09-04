bfd807da5b81cab498a75337a9e656b28035f6e7
Jyotiraditya Panda <jyotiraditya@aospa.co>
2021-09-04 22:01:23 +0200

    Initial HentaiOS bringup
    Signed-off-by: Jyotiraditya Panda <jyotiraditya@aospa.co>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_sweet.mk
+    $(LOCAL_DIR)/hentai_sweet.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_sweet-eng \
-    arrow_sweet-userdebug \
-    arrow_sweet-user
+    hentai_sweet-eng \
+    hentai_sweet-userdebug \
+    hentai_sweet-user
---
+++ hentai_sweet.mk
@@ -0,0 +1,36 @@
+# Inherit from sweet device
+$(call inherit-product, device/xiaomi/sweet/device.mk)
+
+#
+# All components inherited here go to system image
+#
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
+#
+# All components inherited here go to system_ext image
+#
+$(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_system_ext.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_system_ext.mk)
+
+#
+# All components inherited here go to product image
+#
+$(call inherit-product, vendor/hentai/build/product/hentai_product.mk)
+
+#
+# All components inherited here go to vendor image
+#
+# TODO(b/136525499): move *_vendor.mk into the vendor makefile later
+$(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_vendor.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_vendor.mk)
+
+# Inherit some common hentai stuff.
+$(call inherit-product, vendor/hentai/config/common_telephony.mk)
+
+PRODUCT_NAME := hentai_sweet
+PRODUCT_DEVICE := sweet
+PRODUCT_BRAND := Redmi
+PRODUCT_MODEL := Redmi Note 10 Pro
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
